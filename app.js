14002c5ca4c705d0fe6c76f81dd3f9fcca9d93c8
Hhpon <624791164@qq.com>
2018-11-07 14:35:28 +0800

    add all

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ var app = express();
 app.use(express.static('public'));/*可以访问位于public文件夹的文件*/
 app.use(bodyParser.json());
 
+//allow custom header and CORS
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method == 'OPTIONS') {
+        res.send(200); /让options请求快速返回/
+    }
+    else {
+        next();
+    }
+});
+
 app.post('/article', function (req, res) {
     console.log(req.body);
     var content = req.body.content/*把内容从对象中拿出来*/
