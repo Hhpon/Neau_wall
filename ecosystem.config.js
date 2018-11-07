3ebcd28209c5ac9c5afe989397533fa178f7c5c6
Hhpon <624791164@qq.com>
2018-11-07 17:45:49 +0800

    change eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo: 'git@github.com:Hhpon/Neau_wall.git',
       ssh_options: 'StrictHostKeyChecking=no',
       path: '~/www/neau-wall/production',
-      'post-deploy': 'npm install --registry=https://registry.npm.taobao.org && pm2 startOrReload ecosystem.config.js --env production',
+      'post-deploy': 'npm install --registry=https://registry.npm.taobao.org && pm2 startOrRestart ecosystem.config.js --env production',
       'env': {
         NODE_ENV: 'production'
       }
