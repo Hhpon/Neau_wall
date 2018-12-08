5a18c8757d47decae0333ce76e688111aaa4f2ec
lrw-mm <lrw3184669mm@126.com>
2018-12-08 10:59:01 +0800

    repair bug

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');/*express的中间键，把传给http的文本转换为对象供路由调用*/
 var Article = require('./db.js');/*链接数据库*/
+var Note = require('./note-db.js');/*链接数据库*/
 
 var app = express();
 
@@ -37,22 +38,6 @@ app.post('/article', function (req, res) {
     })
 })
 
-
-app.post('/note',function(req,res){
-    var money = req.body.money/*把内容从对象中拿出来*/
-    var time = req.body.time
-    var way = req.body.way
-    let showNote = req.body;
-    Article.create(showNote, function (err, doc) {
-        if (err) {
-            res.end('no');
-        } else {
-            console.log(doc);
-            res.end('ok');
-        }
-    })
-})
-
 app.get('/getArticle', function (req, res) {/*获取数据库中的内容*/
     console.log(req.query);
     let kindof = req.query.value;
@@ -70,39 +55,26 @@ app.get('/getArticle', function (req, res) {/*获取数据库中的内容*/
     })
 })
 
-app.get('/getNote',function(req,res){
-    let income = req.query.income;
-    let date = req.query.date;
-    let use = req.query.use;
-    Article.find( function (err, doc) {
+app.post('/note',function(req,res){
+    var money = req.body.money/*把内容从对象中拿出来*/
+    var time = req.body.time
+    var way = req.body.way
+    let showNote = req.body;
+    Note.create(showNote, function (err, doc) {
+        if (err) {
+            res.end('no');
+        } else {
+            console.log(doc);
+            res.end('ok');
+        }
+    })
+})
+
+app.get('/getNote', function (req, res) {
+    Note.find(function (err, doc) {
         res.json(doc)
     })
 })
-//app.get('/getContent', function (req, res) {/*获取数据库中的内容*/
-    //console.log(req.query);
-    //let income = req.query.income;
-    //let date = req.query.date;
-    ///let use = req.query.use;
-    //Content.find(function (err, doc) {
-       // res.json(doc)       
-    //})
-//})
-
-// app.post('/getArticle', function (req, res) {
-//     console.log(req.body);
-//     let kindof = req.body.kindof;
-//     if (kindof === '全部') {
-//         Article.find(function (err, doc) {
-//             res.json(doc)
-//         })
-//         return;
-//     }
-//     console.log('111')
-//     Article.find({ kindof: kindof }, function (err, doc) {
-//         res.json(doc)
-//     })
-// })
-
 
 app.listen(3000, function () {
     console.log('服务器正在监听3000端口！')
